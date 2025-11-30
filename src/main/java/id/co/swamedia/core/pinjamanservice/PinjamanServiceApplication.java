db95ff82978e4a441edb26e838e1c4705365ca02
bagoes <bagoes.cahya@bts.id>
2025-11-30 08:59:36 +0700

    init

---
+++ src/main/java/id/co/swamedia/core/pinjamanservice/PinjamanServiceApplication.java
@@ -0,0 +1,13 @@
+package id.co.swamedia.core.pinjamanservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinjamanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinjamanServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/swamedia/core/pinjamanservice/PinjamanServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.swamedia.core.pinjamanservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinjamanServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
